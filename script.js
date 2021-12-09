c1af7dc8ad3903ab1cee0b4a02fe39b76b35d8e4
Ivan <pinjamar@gmail.com>
2021-12-09 17:13:34 +0100

    added images, built the navbar and homepage

---
+++ script.js
@@ -0,0 +1,34 @@
+const nav = document.querySelector(".nav");
+window.addEventListener("scroll", fixNav);
+
+function fixNav() {
+  if (window.scrollY > nav.offsetHeight + 150) {
+    nav.classList.add("active");
+  } else {
+    nav.classList.remove("active");
+  }
+}
+
+// const hamburgerButton = document.getElementById("hamburger");
+// const navList = document.getElementById("nav-list");
+
+// function toggleButton() {
+//   navList.classList.toggle("show");
+// }
+
+// hamburgerButton.addEventListener("click", toggleButton);
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
